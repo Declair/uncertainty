2bb648a5ebba2c089ded9dd829dc13717e844aa7
hjt <337991002@qq.com>
2018-05-13 22:24:01 +0800

    【*】注释两个页签

--- uncertainty2/src/MainUI/MainUI.py
+++ uncertainty2/src/MainUI/MainUI.py
@@ -57,14 +57,14 @@ class PlatformForUncertainly(wx.Frame):
         
         #每个页签下加入各功能模块panel
         self.model_panel = ModelUi.ModelPanel(self.statusBar)
-        self.model_panel2 = ModelUi.ModelPanel(self.statusBar)
+#         self.model_panel2 = ModelUi.ModelPanel(self.statusBar)
         self.model_panel3 = UncertaintyPropagationUi.UncertaintyPropagationPanel(self.statusBar)
-        self.model_panel4 = ModelUi.ModelPanel(self.statusBar)
+#         self.model_panel4 = ModelUi.ModelPanel(self.statusBar)
         self.model_panel5 = CalibrationPanel.CalibrationPanel(self.statusBar)
         self.statusBar.AddPage(self.model_panel, u"仿真模型管理", True)
-        self.statusBar.AddPage(self.model_panel2, u"不确定性建模", False)
+#         self.statusBar.AddPage(self.model_panel2, u"不确定性建模", False)
         self.statusBar.AddPage(self.model_panel3, u"不确定性传播分析", False)
-        self.statusBar.AddPage(self.model_panel4, u"仿真验证分析", True)
+#         self.statusBar.AddPage(self.model_panel4, u"仿真验证分析", True)
         self.statusBar.AddPage(self.model_panel5, u"仿真校准", False)
         
         self.main_panel.Layout()

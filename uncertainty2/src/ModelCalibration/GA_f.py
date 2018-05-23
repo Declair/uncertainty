3947af05d2b7b8cb24ebcb3cbfa5dfbf6b0605f4
jinrae <1206619573@qq.com>
2018-05-23 16:23:52 +0800

    0523update

---
+++ uncertainty2/src/ModelCalibration/GA_f.py
@@ -0,0 +1,183 @@
+# -*- coding: utf-8 -*-
+import double_loop
+import numpy
+import matplotlib.pyplot as plt
+import build_meta
+
+avg_dif = list()
+max_dif = list()
+min_dif = list()
+cmp_dif = list()
+best_mat = 1
+sym = 0
+
+def GA_1(cog_p, svr):
+    shape_v = cog_p.shape
+    population_num = shape_v[0]
+    output_l = list()
+    pro_l = list()
+    tot_o = 0
+    tot_dis = 0
+    for i in range(population_num):
+        input_x_v = cog_p[i]
+        # print("输入:", end=" ")
+        # print(input_x_v, end= " ")
+        output_t = svr.predict(input_x_v)
+        # print input_x_v,
+        # print '的预测输出:'
+        # print output_t
+        # print(output_t, end=" ")
+        output_l.append(output_t[0])
+        tot_o = tot_o + 1 / output_t[0]
+        tot_dis = tot_dis + output_t[0]
+
+    # print "平均一致性度量:",
+    # print tot_dis / population_num,
+    # avg_dif.append(tot_dis/population_num)
+    # print("种群大小:", end=" ")
+    # print(population_num, end=" ")
+    sum = 0
+    max = 0
+    min = 2
+    reci = 0
+    recia = 0
+    for i in range(population_num):
+        if max < 1 / output_l[i] / tot_o:
+            max = 1 / output_l[i] / tot_o
+            reci = i
+        if min > 1 / output_l[i] / tot_o:
+            min = 1 / output_l[i] / tot_o
+            recia = i
+        sum = 1 / output_l[i] / tot_o + sum
+        pro_l.append(sum)
+
+    global sym
+    global best_mat
+    if sym == 0:
+        sym=1
+        best_mat = cog_p[reci]
+    else:
+        best_mat = numpy.row_stack((best_mat, cog_p[reci]))
+
+    avg_dif.append(tot_dis / population_num)
+    min_dif.append(output_l[reci])
+    max_dif.append(output_l[recia])
+
+    y_v = double_loop.outer_level_loop(cog_p[reci], build_meta.test_cmp_inh_p, build_meta.test_cmp_output, build_meta.test_cmp_input)
+    cmp_dif.append(y_v[0])
+    return pro_l
+
+def retIndex(pro_l, len, r_v):  # 随机选取一个个体的下标  按照概率的大小
+    i = 0
+    for i in range(len):
+        if pro_l[i] >= r_v:
+            return i
+    return i
+
+def cross_op(individual_1, individual_2):
+    shape_v = individual_1.shape
+    len = shape_v[1]
+    cross_point = numpy.random.randint(1, len)
+    ret_indi = numpy.mat(numpy.full(individual_1.shape, 0))
+    for i in range(shape_v[1]):
+        if i <= cross_point:
+            ret_indi[0, i] = individual_1[0, i]
+        else:
+            ret_indi[0, i] = individual_2[0, i]
+    return ret_indi
+
+def mut_op(indi):
+    shape_v = indi.shape
+    r_v = numpy.random.randint(1, shape_v[1])
+    r_v_a = numpy.random.rand()
+    if r_v_a >= 0.5:
+        indi[0, r_v] = indi[0, r_v] + 1
+    else:
+        indi[0, r_v] = indi[0, r_v] - 1
+    return indi
+
+def GA_2(pro_l, cog_p, cross_p, mut_p):  # 产生新种群
+    shape_v = cog_p.shape
+    num_iter = shape_v[0]
+    row_t = cog_p[retIndex(pro_l, num_iter, numpy.random.rand())]
+    new_cog_p = numpy.mat(numpy.full(row_t.shape, 0))
+    shape_va = row_t.shape
+    for i in range(shape_va[1]):
+        new_cog_p[0, i] = row_t[0, i]
+    i = 1
+    while i <= num_iter - 1:
+        rand_c = numpy.random.rand()
+        if rand_c >= 0 and rand_c < cross_p:  # 交叉成立
+            r_1 = numpy.random.rand()
+            r_2 = numpy.random.rand()
+            index_1 = retIndex(pro_l, num_iter, r_1)
+            index_2 = retIndex(pro_l, num_iter, r_2)
+            while index_2 == index_1:
+                r_2 = numpy.random.rand()
+                index_2 = retIndex(pro_l, num_iter, r_2)
+            individual_1 = cog_p[index_1]
+            individual_2 = cog_p[index_2]
+            new_indi = cross_op(individual_1, individual_2)
+            r_3 = numpy.random.rand()
+            if r_3 >= 0 and r_3 < mut_p:
+                new_indi = mut_op(new_indi)
+            new_cog_p = numpy.row_stack((new_cog_p, new_indi))
+            i = i + 1
+        else:
+            r = numpy.random.rand()
+            index = retIndex(pro_l, num_iter, r)
+            new_indi = cog_p[index]
+            r_3 = numpy.random.rand()
+            if r_3 >= 0 and r_3 < mut_p:
+                new_indi = mut_op(new_indi)
+            new_cog_p = numpy.row_stack((new_cog_p, new_indi))
+            i = i + 1
+    return new_cog_p
+
+def GA(bmodel, pn=100, itn=50, cp=0.3, mp=0.05, cog_p_n=4):
+    population_num = pn  # 种群大小
+    iter_num = itn  # 迭代次数
+    cross_p = cp  # 交叉概率
+    mut_p = mp  # 变异概率
+    print '种群大小: %d' % (population_num),
+    print '迭代次数: %d' % (iter_num),
+    print '交叉概率: %f' % (cross_p),
+    print '变异概率: %f' % (mut_p)
+    cog_p = numpy.mat(numpy.random.rand(population_num, cog_p_n))*9  # 初始种群
+    print '期望最佳预测:'
+    print bmodel.predict([[4,1,8]])
+    for i in range(iter_num):
+        print "第%d次迭代" % (i+1)
+        # print '种群:'
+        # print cog_p
+        pro_l = GA_1(cog_p, bmodel)
+        # print '概率列表:'
+        # print pro_l
+        cog_p = GA_2(pro_l, cog_p, cross_p, mut_p)
+
+    print 'max_dif:'
+    print max_dif
+    print 'avg_dif:'
+    print(avg_dif)
+    print 'min_dif:'
+    print min_dif
+    print 'cmp_dif:'
+    print(cmp_dif)
+    print 'best_mat:'
+    print best_mat
+
+    plt.figure(num=1, figsize=(6, 3))
+    x = len(avg_dif)
+    x= range(x)
+    plt.plot(x, avg_dif)
+    plt.plot(x, max_dif)
+    plt.plot(x, min_dif)
+    plt.xlabel('iter num')
+    plt.ylabel('measure size')
+    plt.title('average measure size trend')
+    plt.figure(num=2, figsize=(6, 3))
+    plt.plot(x, cmp_dif)
+    plt.xlabel('iter num')
+    plt.ylabel('measure size')
+    plt.title('verify trend')
+    plt.show()
---
+++ uncertainty2/src/ModelCalibration/build_meta.py
@@ -0,0 +1,144 @@
+# -*- coding: utf-8 -*-
+import numpy
+import real_model as rm
+import double_loop
+from sklearn import svm
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn import linear_model
+from sklearn.model_selection import train_test_split
+
+
+test_cog_p = 1
+test_inh_p = 1
+test_cmp_inh_p =1
+
+test_input = 1
+test_cmp_input = 1
+
+test_output = 1
+test_cmp_output = 1
+
+
+def initData(cog_p_gn=400, cog_p_n=4, inh_p_gn=20, inh_p_n=1, c_data_n=30, cmp_data_n=20):
+    global test_cog_p
+    test_cog_p = numpy.mat(numpy.random.rand(cog_p_gn, cog_p_n))*9  # 认知不确定参数 实际应该通过抽样获得
+
+    global test_inh_p
+    test_inh_p = numpy.mat(numpy.random.standard_normal(size=(inh_p_gn, inh_p_n)))  # 固有不确定参数  实际应该通过抽样获得
+    global test_cmp_inh_p
+    test_cmp_inh_p = test_inh_p
+
+    global test_input
+    test_input = numpy.mat(
+        numpy.random.rand(c_data_n, 3))*5+1  # 这个输入是为了让实际系统和仿真系统在不确定参数确定的情况下获得相应的输出，进而可以比较获得马氏距离，进而来训练SVR模型
+
+    global test_cmp_input
+    test_cmp_input = numpy.mat(
+        numpy.random.rand(cmp_data_n, 3))*5+1 # 这个输入是在每一次优化中获得的最优参数下仿真模型的输出和实际系统在这个输入下的输出的比较，进而可以看见优化参数的效果
+
+    global test_output
+    test_output = rm.run_real_model(test_inh_p, test_input)  # 为了获取一致性度量结果
+
+    global test_cmp_output  # 为了比较验证仿真校准结果
+    test_cmp_output = rm.run_real_model(test_inh_p, test_cmp_input)
+
+def buildSVR1(test_cog_p, test_inh_p, test_output, test_input, cus_C, cus_epsilon, cus_kernel):
+    #print("SVR建模方法，C: %f，epsilon：%f，kernel：%s"%(cus_C, cus_epsilon, cus_kernel))
+
+    print('认知不确定参数矩阵:')
+    print(test_cog_p)
+    print('固有不确定参数')
+    print(test_inh_p)
+    print('参考输入:')
+    print(test_input)
+    print('参考输出矩阵:')
+    print(test_output)
+    print('对比输入:')
+    print(test_cmp_input)
+    print('对比输出矩阵')
+    print(test_cmp_output)
+    y_v = double_loop.outer_level_loop(test_cog_p, test_inh_p, test_output, test_input)  # 运行仿真系统获得输出向量，即马氏距离的向量  该输出和认知不确定参数Es_p共同构成训练数据集
+    y_va = numpy.array(y_v)
+    print('训练输出:')
+    print(y_va)
+
+    print('最佳参数取值对应输出:')
+    best_p = numpy.mat([[4,1,8]])
+    y_vaa = double_loop.outer_level_loop(best_p, test_inh_p, test_output, test_input)
+    print(y_vaa)
+
+    test_cog_pa = numpy.array(test_cog_p)
+
+    X_train, X_test, y_train, y_test = train_test_split(test_cog_pa, y_va, test_size = .4, random_state = 0)
+    svr = svm.SVR(kernel=cus_kernel, C=cus_C, epsilon=cus_epsilon).fit(X_train, y_train)
+
+    print 'score:'
+    print svr.score(X_test, y_test)
+
+    return svr
+
+def buildSVR2(test_cog_p, test_inh_p, test_output, test_input, cus_alpha):
+    print ("GPR建模方法，alpha：%f"%(cus_alpha))
+    print('认知不确定参数矩阵:')
+    print(test_cog_p)
+    print('固有不确定参数')
+    print(test_inh_p)
+    print('参考输入:')
+    print(test_input)
+    print('参考输出矩阵:')
+    print(test_output)
+    print('对比输入:')
+    print(test_cmp_input)
+    print('对比输出矩阵')
+    print(test_cmp_output)
+    y_v = double_loop.outer_level_loop(test_cog_p, test_inh_p, test_output, test_input)  # 运行仿真系统获得输出向量，即马氏距离的向量  该输出和认知不确定参数Es_p共同构成训练数据集
+    y_va = numpy.array(y_v)
+    print('训练输出:')
+    print(y_va)
+
+    print('最佳参数取值对应输出:')
+    best_p = numpy.mat([[4, 1, 8]])
+    y_vaa = double_loop.outer_level_loop(best_p, test_inh_p, test_output, test_input)
+    print(y_vaa)
+
+    test_cog_pa = numpy.array(test_cog_p)
+
+    X_train, X_test, y_train, y_test = train_test_split(test_cog_pa, y_va, test_size=.4, random_state=0)
+    gpr = GaussianProcessRegressor(alpha=cus_alpha).fit(X_train, y_train)
+
+    print 'score:'
+    gpr.score(X_test, y_test)
+    return gpr
+
+def buildSVR3(test_cog_p, test_inh_p, test_output, test_input, cus_n_iter, cus_tol ):
+    print("Bayes建模方法，iter：%d，tol：%d"%(cus_n_iter, cus_tol))
+    print('认知不确定参数矩阵:')
+    print(test_cog_p)
+    print('固有不确定参数')
+    print(test_inh_p)
+    print('参考输入:')
+    print(test_input)
+    print('参考输出矩阵:')
+    print(test_output)
+    print('对比输入:')
+    print(test_cmp_input)
+    print('对比输出矩阵')
+    print(test_cmp_output)
+    y_v = double_loop.outer_level_loop(test_cog_p, test_inh_p, test_output, test_input)  # 运行仿真系统获得输出向量，即马氏距离的向量  该输出和认知不确定参数Es_p共同构成训练数据集
+    y_va = numpy.array(y_v)
+    print('训练输出:')
+    print(y_va)
+
+    print('最佳参数取值对应输出:')
+    best_p = numpy.mat([[4, 1, 8]])
+    y_vaa = double_loop.outer_level_loop(best_p, test_inh_p, test_output, test_input)
+    print(y_vaa)
+
+    test_cog_pa = numpy.array(test_cog_p)
+
+    X_train, X_test, y_train, y_test = train_test_split(test_cog_pa, y_va, test_size=.4, random_state=0)
+    bayes = linear_model.BayesianRidge(n_iter=cus_n_iter, tol=cus_tol).fit(X_train, y_train)
+
+    print 'score:'
+    print bayes.score(X_test, y_test)
+    return bayes

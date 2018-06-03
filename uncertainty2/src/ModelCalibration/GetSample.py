a1b9a0d8721ae5300dacc3ee07c7b34bee280e28
hjt <337991002@qq.com>
2018-06-03 18:47:28 +0800

    【*】抽样只能171条bug

--- uncertainty2/src/ModelCalibration/GetSample.py
+++ uncertainty2/src/ModelCalibration/GetSample.py
@@ -4,12 +4,46 @@ import Sql
 import numpy as np
 
 #arg_type = 0取自变量   1取固有  2是认知
-def get_samp(nid = 9, arg_type = 1):
+def get_samp(nid = 9, arg_type = 2):
+    count = Sql.selectSql((nid, arg_type), Sql.get_sampling_count)
+    count = count[0][0]
+    records = Sql.selectSql((nid, arg_type), Sql.get_samp1)
+    records = [record[1] for record in records]
+    print records
+    samps = [records[i:i + count] for i in range(0, len(records), count)]
+    flag = 0
+    for samp in samps:
+        if flag == 0:
+            mat = samp
+            flag = 1
+        else:
+            mat = np.row_stack((mat, samp))
+    mat = np.transpose(mat)
+    mat = np.mat(mat)
+    return mat
+    
+    
+    
+#     flag = 0
+#     
+#         if flag == 0:
+#             mat = samps
+#             flag = 1
+#         else:
+#             mat = np.row_stack((mat, samps))
+# #     mat = np.transpose(mat)
+# 
+#     mat = np.mat(mat)
+# 
+#     return mat
+
+def get_samp2(nid = 9, arg_type = 1):
     records = Sql.selectSql((nid, arg_type), Sql.get_samp2)
     flag = 0
     for record in records:
         samps = record[1].decode('utf-8')
         samps = samps.split(',')
+        print len(samps)
         samps = [float(samp) for samp in samps]
         if flag == 0:
             mat = samps
@@ -19,8 +53,8 @@ def get_samp(nid = 9, arg_type = 1):
     mat = np.transpose(mat)
 
     mat = np.mat(mat)
-    print mat
-    print mat.shape
+#     print mat
+#     print mat.shape
     # print type(mat)
     # print mat.shape
     # tmat =  mat[:, 0:1]
@@ -31,4 +65,4 @@ def get_samp(nid = 9, arg_type = 1):
     return mat
 
 if __name__ == '__main__':
-    get_samp(arg_type = 0)
+    get_samp()
